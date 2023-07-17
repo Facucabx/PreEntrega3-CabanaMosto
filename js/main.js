d07aa9beedb42998992c63021cd007181b14e2d7
Your Name <you@example.com>
2023-07-17 00:02:00 -0300

    probando

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+// BUSCADOR DE SERVICIOS 
+function buscarServicio() {
+    var input = document.querySelector('.buscar');
+    var servicios = document.querySelectorAll('.card-servicios');
+
+    var searchTerm = input.value.toLowerCase();
+
+    servicios.forEach(function (servicio) {
+        var nombre = servicio.querySelector('h3').textContent.toLowerCase();
+        var profesion = servicio.querySelector('p').textContent.toLowerCase();
+
+        if (nombre.includes(searchTerm) || profesion.includes(searchTerm)) {
+            servicio.style.display = 'block';
+        } else {
+            servicio.style.display = 'none';
+        }
+    });
+}
+
+// BUSCADOR DE EVENTOS
 function buscarEvento() {
     var input = document.querySelector('.buscar');
     var eventos = document.querySelectorAll('.card-evento');
@@ -23,4 +43,37 @@ function buscarEvento() {
     } else {
         sinResultados.style.display = 'block';
     }
+}
+
+
+// index 
+
+function buscarServicio() {
+    var input = document.querySelector('.buscar');
+    var servicios = document.querySelectorAll('.card-servicios');
+    var eventos = document.querySelectorAll('.card-evento');
+
+    var searchTerm = input.value.toLowerCase();
+
+    servicios.forEach(function (servicio) {
+        var nombre = servicio.querySelector('h3').textContent.toLowerCase();
+        var profesion = servicio.querySelector('p').textContent.toLowerCase();
+
+        if (nombre.includes(searchTerm) || profesion.includes(searchTerm)) {
+            servicio.style.display = 'block';
+        } else {
+            servicio.style.display = 'none';
+        }
+    });
+
+    eventos.forEach(function (evento) {
+        var nombre = evento.querySelector('h3').textContent.toLowerCase();
+        var lugar = evento.querySelector('p').textContent.toLowerCase();
+
+        if (nombre.includes(searchTerm) || lugar.includes(searchTerm)) {
+            evento.style.display = 'block';
+        } else {
+            evento.style.display = 'none';
+        }
+    });
 }
